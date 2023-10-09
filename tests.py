f56ea92f9fd662f5f3e5bcf5d92f14a96c34f211
Sergey <dev.vilfer@gmail.com>
2023-10-09 22:30:56 +0300

    remove comments

--- tests.py
+++ tests.py
@@ -24,16 +24,6 @@ def test_simplex2():
     assert np.allclose(res[0], np.array([0, 0, 2]))
     assert np.allclose(res[1], -2)
 
-# def test_simplex3():
-#     a = np.array([[0, 0], [0, 0], [0, 0]])
-#     b = np.array([0, 0])
-#     c = np.array([0, 0])
-#     epsilon = 1e-6
-#     x, z = simplex(a, b, c, epsilon)
-#     z = np.dot(c, x)
-#     assert np.allclose(x, np.array([0, 0]))
-#     assert np.allclose(z, 0)
-
 def test_simplex3():
     a = np.array([[1, 1], [1, 1]])
     b = np.array([1, 1])
@@ -52,26 +42,6 @@ def test_simplex4():
     assert np.allclose(res[0], np.array([2]))
     assert np.allclose(res[1], -4)
 
-# def test_simplex5():
-#     a = np.array([[1, -3], [-2, -1], [11, -0.5]])
-#     b = np.array([4, 2, -7])
-#     c = np.array([2, 3])
-#     epsilon = 1e-6
-#     res = calculateSimplex(a, b, c, epsilon)
-#     print("teeeee", res)
-#     assert np.allclose(res[0], np.array([0, 14]))
-#     assert np.allclose(res[1], 42)
-
-# def test_simplex6():
-#     a = np.array([[1, -3], [-2, -1], [11, -0.5]])
-#     b = np.array([4, 2, -7])
-#     c = np.array([2, 3])
-#     epsilon = 1e-6
-#     res = calculateSimplex(a, b, c, epsilon)
-#     print("teeeee", res)
-#     assert np.allclose(res[0], np.array([0, 14]))
-#     assert np.allclose(res[1], 42)
-
 def test_simplex7():
     a = np.array([[-1], [2]])
     b = np.array([3, 10])
@@ -83,29 +53,3 @@ def test_simplex7():
     res = calculateSimplex(a, b, c, epsilon)
     assert np.allclose(res[0], np.array([5]))
     assert np.allclose(res[1], -10)
-
-
-
-# cases with exceptions:
-
-# (1)
-# Enter the coefficients of the objective function: -2
-# Enter the size of matrix A (Example: 3 4): 1 1
-# Enter the coefficients of constraint functions:
-# -1
-# Enter the right-hand side numbers of constraint functions: 3
-# Enter the approximation accuracy: 1e-6
-# The method is not applicable!
-# PASSED
-
-
-# (2)
-# Enter the coefficients of the objective function: -2
-# Enter the size of matrix A (Example: 3 4): 2 1
-# Enter the coefficients of constraint functions:
-# -1
-# 2
-# Enter the right-hand side numbers of constraint functions: 3 5 6
-# Enter the approximation accuracy: 1e-6
-# The method is not applicable!
-# PASSED
